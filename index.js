b27a95664ade1a2d3a344dac06176c198aa6e183
kkush <kkush33@outlook.com>
2017-09-29 16:30:46 +0530

    auth flow completed

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+console.log(keys.mongoURI)
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({'hello' : 'world'});
-})
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+// require('./routes/authRoutes')(app) // alternative way to do it
 
 const PORT = process.env.PORT || 5000;
 
---
+++ services/passport.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user =>{
+            done(null, user);
+        });
+});
+
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done ) => {
+            User.findOne(
+                {googleId: profile.id}
+            ).then((exsistingUser) => {
+                if(exsistingUser){
+                    // We already have a record with given profile Id
+                    console.log(exsistingUser)
+                    done(null,exsistingUser);
+                } else {
+                    new User({
+                        googleId: profile.id
+                    }).save().then(user => {
+                        done(null, user)
+                    })
+                }
+            })
+
+
+            // console.log('access Token :', accessToken)
+            // console.log('refresh Token :', refreshToken)
+            // console.log('profile :', profile)
+        }
+    )
+);
